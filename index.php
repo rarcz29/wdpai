b155db9014973cc7465b18ca803ed0e681a68550
rarcz29 <rafalcz29@gmail.com>
2020-10-27 14:02:05 +0100

    Update

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-
-echo "XD";
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CodeCrew</title>
+    <meta name="description" content="WdPAI project">
+    <meta name="keywords" content="code programming team crew">
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <header>
+        <div id="logo">CodeCrew</div>
+        <nav>
+            <ul class="navbar">
+                <li><a href="default.asp">Home</a></li>
+                <li><a href="news.asp">News</a></li>
+                <li><a href="contact.asp">Contact</a></li>
+                <li><a href="about.asp">About</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
