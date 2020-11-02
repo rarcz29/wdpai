30758f2139e437179383a66ee7d7f558ba501082
rarcz29 <rafalcz29@gmail.com>
2020-11-02 18:39:09 +0100

    Add code to create repo on github

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CodeCrew</title>
-    <meta name="description" content="WdPAI project">
-    <meta name="keywords" content="code programming team crew">
-    <link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-    <header class="topbar">
-        <div class="logo">CodeCrew</div>
-        <div>
-            <nav class="nav-h">
-                <ul>
-                    <li><a href="#x">Home</a></li>
-                    <li><a href="#X">News</a></li>
-                    <li><a href="#X">Contact</a></li>
-                    <li><a href="#X">About</a></li>
-                </ul>
-            </nav>
-            <div>user</div>
-        </div>
-    </header>
-</body>
-</html>
+<?php
+
+$url = "https://api.github.com/user/repos";
+$username = "xxxxxx";
+$password = "xxxxxx";
+$auth_data = base64_encode("${username}:${password}");
+
+$curl = curl_init();
+
+$headers = array(
+    "User-Agent: ${username}",
+    "Authorization: Basic ${auth_data}",
+    "Accept: application/vnd.github.v3+json"
+);
+
+$post_fields = array(
+    "name" => "asdfasdf"
+);
+
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_POST, count($post_fields));
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post_fields));
+
+$output = curl_exec($curl);
+curl_close($curl);
+
+echo $output;
