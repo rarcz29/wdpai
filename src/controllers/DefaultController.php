dc26c8b908b31057936b37aef5ae2ec1ca25cf3a
rarcz29 <rafalcz29@gmail.com>
2020-12-01 15:50:46 +0100

    Update paths to files

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends AppController {
         $this->render('login');
     }
 
+    public function signup() {
+        $this->render('signup');
+    }
+
     public function home() {
         $this->render('home');
     }
