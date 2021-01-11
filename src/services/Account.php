b8bd50d21f100f4b6753e12f6e0878cb2980b701
rarcz29 <rafalcz29@gmail.com>
2021-01-11 11:59:03 +0100

    Checking privileges

--- src/services/Account.php
+++ src/services/Account.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__.'/Cookies.php';
 require_once __DIR__.'/Session.php';
+require_once __DIR__.'/../repository/UserRolesRepository.php';
+require_once __DIR__.'/../models/UserRoles.php';
 
 // TODO: add session to the database
 class Account
@@ -10,27 +12,35 @@ class Account
     {
         Cookies::setNicknameCookie($username);
         Session::setUserId($id);
+        $rolesRepo = new UserRolesRepository();
+        $roles = $rolesRepo->getRoles();
+        Session::setAdminPrivileges($roles->isAdmin());
+        Session::setModeratorPrivileges($roles->isModerator());
     }
 
     public function logOut()
     {
         Cookies::deleteCookies();
-        Session::unsetUserId();
+        Session::unsetAll();
     }
 
     public function isLoggedIn(): bool
     {
         return Cookies::getNickname() !== null &&
-            Session::getUserId() !== null;
+            Session::getUserId() !== null &&
+            Session::isAdmin() !== null &&
+            Session::isModerator() !== null;
     }
 
     public function getUserId(): ?int
     {
+        $this->logOutIfNoData();
         return Session::getUserId();
     }
 
     public function getUserName(): ?string
     {
+        $this->logOutIfNoData();
         return Cookies::getNickname();
     }
 
@@ -39,6 +49,18 @@ class Account
         if ($this->isLoggedIn())
         {
             Cookies::setNicknameCookie(Cookies::getNickname());
+        }
+        else
+        {
+            $this->logOut;
+        }
+    }
+
+    private function logOutIfNoData()
+    {
+        if (!$this->isLoggedIn())
+        {
+            $this->logOut();
         }
     }
 }
