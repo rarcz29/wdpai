268df59ad209ac8e3a50821f1cf38bb2a2e9f72f
rarcz29 <rafalcz29@gmail.com>
2021-01-18 10:57:13 +0100

    Responsive navbar menu

--- public/views/shared/_header.php
+++ public/views/shared/_header.php
@@ -3,7 +3,7 @@
 
     <div class="nav-container flex-center">
         <nav>
-            <ul class="list">
+            <ul class="list main-list">
                 <li><a href="home">Home</a></li>
                 <li><a href="community">Community</a></li>
                 <li><a href="#">About</a></li>
@@ -15,12 +15,19 @@
                     }
                 ?>
             </ul>
+
+            <ul class="list user-list">
+                <li><a href="#">Settings</a></li>
+                <li><a href="#">Sign out</a></li>
+            </ul>
         </nav>
 
         <div class="right-side-icons flex-center">
-            <i class="fas fa-search"></i>
-            <i class="fas fa-bell"></i>
+<!--            <i class="fas fa-search"></i>-->
+<!--            <i class="fas fa-bell"></i>-->
             <a href="login"><i class="fas fa-user-circle"></i></a>
         </div>
     </div>
+
+    <i class="fas fa-bars burger"></i>
 </header>
